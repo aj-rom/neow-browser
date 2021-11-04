08d9a68fd6b1396fbf04c0579ad4294ed2c3f1a3
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-03 22:11:49 -0600

    add application store (redux & thunk)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 
+import { Provider } from 'react-redux'
+import thunk from "redux-thunk";
+import { createStore, compose, applyMiddleware } from "@reduxjs/toolkit";
+import { searchReducer } from "./reducers/searchReducer";
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store = createStore(searchReducer, composeEnhancers(applyMiddleware(thunk)))
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Provider store={store}>
+          <Component {...pageProps} />
+      </Provider>
+  )
 }
 
 export default MyApp
