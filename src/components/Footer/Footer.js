6290d063b6d9401b21a682246e582aaa39a7ee6b
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-04 12:09:49 -0600

    Fix Source code link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import footerStyles from "./Footer.module.css";
 export default function Footer() {
     return (
         <footer className={footerStyles.footer}>
-            Made by <a href='https://github.com/aj-rom'>A.J. Romaniello</a> | <a href='https://github.com/aj-rom/neo-browser'>Source Code</a>
+            Made by <a href='https://github.com/aj-rom'>A.J. Romaniello</a> | <a href='https://github.com/aj-rom/neow-browser'>Source Code</a>
             <br/>
             API Services by <a href='https://github.com/SpaceRocks/'>The SpaceRocks Team</a>
         </footer>
