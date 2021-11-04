ff52b35914ef51db5363efbea92bcce561b97b17
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-03 22:12:49 -0600

    add state management to search feature

---
+++ pages/actions/SearchActions.js
@@ -0,0 +1,25 @@
+
+export const fetchNeo = data => {
+    const api_key = 'CGRUzhnJsD9iW8xmbF24CYVSCtrMuUtqgixYhQrx'
+    const { start_date, end_date } = data
+
+    const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${end_date}&api_key=${api_key}`
+    return (dispatch) => {
+        dispatch({type: 'LOADING'})
+        function handleResponse(json) {
+            if (json.error || json.code === 400) {
+                if (json.code === 400) {
+                    return dispatch({type: 'ERROR', payload: 'Date range can only span 7 days.'})
+                }
+
+                return dispatch({ type: 'ERROR', payload: json.error.message })
+            } else {
+                dispatch({ type: 'SET_RESULTS', payload: json.near_earth_objects })
+            }
+        }
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(json => handleResponse(json))
+    }
+}
---
+++ pages/reducers/searchReducer.js
@@ -0,0 +1,14 @@
+const initialState = { error: '', isLoading: false, results: [] }
+export const searchReducer = ( state = initialState, action ) => {
+    switch (action.type) {
+        case 'LOADING':
+            return { ...state, isLoading: true}
+        case 'SET_RESULTS':
+            return { ...state, isLoading: false, results: action.payload, error: '' }
+        case 'ERROR':
+            console.log('An error has occurred:', action.error)
+            return { ...state, error: action.payload, isLoaded: false }
+        default:
+            return state
+    }
+}
