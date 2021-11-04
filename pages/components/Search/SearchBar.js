9462b37d85b9e90e8ec4aacb12a02dbc43faab79
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-03 22:13:01 -0600

    create SearchBar.js

--- pages/components/Search/SearchBar.js
+++ pages/components/Search/SearchBar.js
@@ -1,51 +1,56 @@
-
 import styles from './SearchBar.module.css'
-import { useState } from "react";
 
-export default function SearchBar() {
+import {Component} from "react";
+import { fetchNeo } from "../../actions/SearchActions";
+import { connect } from "react-redux";
 
-    const [startDate, setStartDate] = useState('')
-    const [endDate, setEndDate] = useState('')
+class SearchBar extends Component {
 
-    const handleChange = e => {
+    state = { start_date: '', end_date: ''}
+
+    handleChange = e => {
         const { id, value } = e.target
-        console.log(value)
-        if (id === 'startDate') {
-            setStartDate(value)
+        if (id === 'start_date') {
+           this.setState({ start_date: value })
         } else {
-            setEndDate(value)
+            this.setState({ end_date: value })
         }
     }
 
-    const handleSubmit = e => {
+    handleSubmit = e => {
         e.preventDefault()
+        this.props.fetchNeo({ start_date: this.state.start_date, end_date: this.state.end_date })
     }
 
-    return (
-        <form className={styles.form}>
-            <div>
-                <label htmlFor='start_date' className={styles.label}>Start Date</label>
-                <input
-                    type='date'
-                    id='startDate'
-                    className={styles.input}
-                    value={startDate}
-                    onChange={e => handleChange(e)}
-                />
-            </div>
-            <br/>
-            <div>
-                <label htmlFor='end_date' className={styles.label}>End Date</label>
-                <input
-                    type='date'
-                    id='endDate'
-                    className={styles.input}
-                    value={endDate}
-                    onChange={handleChange}
-                />
-            </div>
-            <br/>
-            <input type='submit' className={styles.submit} value='Search' onSubmit={e => handleSubmit(e)}/>
-        </form>
-    )
+    render() {
+        return (
+            <form className={styles.form}>
+                <div>
+                    <label htmlFor='start_date' className={styles.label}>Start Date</label>
+                    <input
+                        type='date'
+                        id='start_date'
+                        className={styles.input}
+                        value={this.state.start_date}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <br/>
+                <div>
+                    <label htmlFor='end_date' className={styles.label}>End Date</label>
+                    <input
+                        type='date'
+                        id='end_date'
+                        className={styles.input}
+                        value={this.state.end_date}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <br/>
+                <input type='button' className={styles.submit} value='Search' onClick={this.handleSubmit}/>
+            </form>
+        )
+    }
 }
+
+export default connect(null, { fetchNeo })(SearchBar)
