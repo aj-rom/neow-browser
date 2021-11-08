ead5fe21eae127072e95a5261e552951afbe9433
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-08 11:45:26 -0700

    create dynamic Meta component

---
+++ src/components/Meta/Meta.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Meta(props) {
+
+    function getMeta() {
+        const { meta } = props
+
+        return Object.entries(meta).map( (arr, idx) => {
+            return <meta key={idx} name={arr[0]} content={arr[1]}/>
+        })
+    }
+
+    return (
+        <Head>
+            <title>NeoW Browser - {props.title}</title>
+            {getMeta()}
+        </Head>
+    )
+}
