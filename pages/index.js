64c0cf5f9243cc457b780f0f614f950e70bd7b21
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-08 11:48:17 -0700

    switch to home page layout, add page meta

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import styles from '../src/styles/Home.module.css'
-import SearchContainer from "../src/containers/Search/SearchContainer";
-import Footer from "../src/components/Footer/Footer";
-import PageHead from "../src/components/Head/Head";
+import Link from 'next/link'
+import Meta from "../src/components/Meta/Meta";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-        <PageHead/>
-        <main className={styles.main}>
-            <div className={styles.header}>
-                <h1>
-                    <b className={styles.b}>NeoW Browser</b>
-                </h1>
-                <p>Search for objects currently near Earth.</p>
-            </div>
-            <SearchContainer/>
-        </main>
-        <Footer/>
-    </div>
-  )
+
+    const pageMeta = {
+        description: "A browser for today's current Near Earth Object. API's provided by NASA's JPL Team",
+        keywords: "Asteroid, NEO, NASA, JPL, ..."
+    }
+
+    return (
+        <div className={styles.container}>
+            <Meta title='Home' meta={pageMeta}/>
+            <main className={styles.main}>
+                <div className={styles.header}>
+                    <h1>
+                        <b className={styles.b}>Browser</b>
+                    </h1>
+                    <p>Detailed day by day browser of Near Earth Objects</p>
+                </div>
+                <Link href={{pathname: '/feed'}}>Feed</Link>
+            </main>
+        </div>
+    )
 }
