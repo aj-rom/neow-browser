fbb6164d39e1c6d5f6499300ffdc1a72c698989c
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-08 11:46:00 -0700

    move feed to it's own page

---
+++ pages/feed.js
@@ -0,0 +1,28 @@
+import Meta from "../src/components/Meta/Meta";
+import styles from "../src/styles/Home.module.css";
+import SearchContainer from "../src/containers/Search/SearchContainer";
+
+
+export default function Feed() {
+
+    const meta = {
+        description: 'Test'
+    }
+
+    return (
+        <div className={styles.container}>
+
+            <Meta title='Feed' meta={meta}/>
+            <main className={styles.main}>
+                <div className={styles.header}>
+                    <h1>
+                        <b className={styles.b}>NeoW Browser</b>
+                    </h1>
+                    <p>Search for objects currently near Earth.</p>
+                </div>
+                <SearchContainer/>
+            </main>
+
+        </div>
+    )
+}
