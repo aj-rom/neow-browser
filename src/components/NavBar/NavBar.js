1b7aebb8bf7977ea02831703c22ec50cec94d5b8
A.J. Romaniello <ajiellodev@gmail.com>
2021-12-01 04:32:15 -0700

    add navbar style

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-import styles from './NavBar.module.css'
 import NavLink from "./NavLink";
 
 export default function NavBar() {
@@ -11,11 +10,19 @@ export default function NavBar() {
     const { browser, feed } = pageUrls
 
     return (
-        <nav className={styles.navbar}>
-            <ul className={styles.links}>
-                <NavLink link={browser}>Browser</NavLink>
-                <NavLink link={feed}>Feed</NavLink>
-            </ul>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <a className='navbar-brand'>NEO Browser</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#collapsable" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+            </button>
+            <div className='collapse navbar-collapse' id='collapsable'>
+                <ul className='navbar-nav mr-auto'>
+                    <NavLink link={browser}>Browser</NavLink>
+                    <NavLink link={feed}>Feed</NavLink>
+                </ul>
+            </div>
         </nav>
     )
 }
